5c2928e4dc19fa8b9da13b2390b9824893b644a3
Martyn Chong <martyncqt@gmail.com>
2020-08-17 02:09:48 +0800

    Before Room Search works, also change Title column

--- database/urls.py
+++ database/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('exams/', views.exams, name='database-exams'),
     path('search/', views.search, name='database-search'),
     path('rooms/', views.rooms, name='database-rooms'),
+    path('roomsearch/', views.rooms, name='database-roomsearch'),
 ]
--- database/views.py
+++ database/views.py
@@ -8,7 +8,7 @@ from jinja2 import Template
 def home(request):
     allexams = Exam.objects.all()
     context={'allexams': allexams}
-    return render(request,'database/home.html',context, )
+    return render(request,'database/home.html',context,)
 
 def exams(request):
     allexams = Exam.objects.all()
@@ -29,18 +29,19 @@ def rooms(request):
     examrooms = ExamRooms()
     ExamRooms.determine_exam_pairings(examrooms)
     zipped = zip(examrooms.listExams,examrooms.listRooms)
-
     context={'listexamsandrooms': zipped
         }
     return render(request, 'database/rooms.html',context)
 
-# def roomsearch(request):
-#     template = 'database/rooms.html'
+def roomsearch(request):
+    template = 'database/rooms.html'
+    examrooms = ExamRooms()
+    ExamRooms.determine_exam_pairings(examrooms)
+    zipped = zip(examrooms.listExams,examrooms.listRooms)
 
-#     query = request.GET.get('q')
-#     results = Exam.objects.filter(Q(title__icontains=query) | Q(code__icontains=query) | Q(subject__icontains=query) | Q(times__icontains=query) | Q(date__icontains=query) | Q(duration__icontains=query))
-#     print(results)
-#     context={'listexamsandrooms': results
-#         }
+    query = request.GET.get('q')
+    results = [t for t in zipped if t[1] == query]
+    context={'listexamsandrooms': results
+        }
 
-#     return render(request, template, context)
+    return render(request, template, context)
