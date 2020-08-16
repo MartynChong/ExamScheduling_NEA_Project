157eef0d6cf82268e43b297b7d7fc8a51735cea4
Martyn Chong <martyncqt@gmail.com>
2020-08-17 01:49:39 +0800

    PRE RoomSearch, RoomAsObject

--- database/urls.py
+++ database/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='database-home'),
     path('exams/', views.exams, name='database-exams'),
-    path('search/', views.search, name='database-search')
+    path('search/', views.search, name='database-search'),
+    path('rooms/', views.rooms, name='database-rooms'),
 ]
--- database/views.py
+++ database/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Exam
+from .models import Exam, ExamRooms
 from django.db.models import Q
+from jinja2 import Template
 
 
 def home(request):
@@ -23,3 +24,23 @@ def search(request):
     context= {'allexams':results}
 
     return render(request, template, context)
+
+def rooms(request):
+    examrooms = ExamRooms()
+    ExamRooms.determine_exam_pairings(examrooms)
+    zipped = zip(examrooms.listExams,examrooms.listRooms)
+
+    context={'listexamsandrooms': zipped
+        }
+    return render(request, 'database/rooms.html',context)
+
+# def roomsearch(request):
+#     template = 'database/rooms.html'
+
+#     query = request.GET.get('q')
+#     results = Exam.objects.filter(Q(title__icontains=query) | Q(code__icontains=query) | Q(subject__icontains=query) | Q(times__icontains=query) | Q(date__icontains=query) | Q(duration__icontains=query))
+#     print(results)
+#     context={'listexamsandrooms': results
+#         }
+
+#     return render(request, template, context)
